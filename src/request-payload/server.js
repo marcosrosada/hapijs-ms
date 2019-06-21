d1f082475fb78af4fa2854f8bd55de2782317516
Marcos Rosada <ochild@gmail.com>
2019-06-21 12:21:44 -0300

    Request Payload

---
+++ src/request-payload/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'POST',
+        path:'/',
+        handler: (request, reply) => {
+            const payload = request.payload;
+
+            return payload;
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
